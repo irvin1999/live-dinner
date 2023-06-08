fce1d560068b62d8c6cf320e3efdde8a7e57fe26
irvin99 <54644054+irvin1999@users.noreply.github.com>
2023-06-08 15:13:35 +0200

    adasds

--- php/form-process.php
+++ php/form-process.php
@@ -1,78 +1,29 @@
 <?php
-
-$errorMSG = "";
-
-// NAME
-if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
-} else {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recibe los datos del formulario
+    $date = $_POST["date"];
+    $time = $_POST["time"];
+    $person = $_POST["person"];
     $name = $_POST["name"];
-}
-
-// EMAIL
-if (empty($_POST["email"])) {
-    $errorMSG .= "Email is required ";
-} else {
     $email = $_POST["email"];
-}
+    $phone = $_POST["phone"];
 
-// MSG Guest
-if (empty($_POST["guest"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $guest = $_POST["guest"];
-}
+    // Configura los detalles del correo electrónico
+    $to = "tucorreo@example.com";
+    $subject = "Reserva de mesa";
+    $message = "Fecha: " . $date . "\n";
+    $message .= "Hora: " . $time . "\n";
+    $message .= "Número de personas: " . $person . "\n";
+    $message .= "Nombre: " . $name . "\n";
+    $message .= "Correo electrónico: " . $email . "\n";
+    $message .= "Teléfono: " . $phone . "\n";
+    $headers = "From: " . $email;
 
-
-// MSG Event
-if (empty($_POST["event"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $event = $_POST["event"];
-}
-
-
-// MESSAGE
-if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
-} else {
-    $message = $_POST["message"];
-}
-
-
-$EmailTo = "11snaider99@gmail.com";
-$Subject = "New Message Received";
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "guest: ";
-$Body .= $guest;
-$Body .= "\n";
-$Body .= "event: ";
-$Body .= $event;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
+    // Envía el correo electrónico
+    if (mail($to, $subject, $message, $headers)) {
+        echo "¡Gracias por reservar! Te contactaremos pronto.";
     } else {
-        echo $errorMSG;
+        echo "Error al enviar el formulario. Por favor, inténtalo de nuevo.";
     }
 }
-
 ?>
