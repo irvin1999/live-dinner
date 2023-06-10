c73f317dbabc311f2f8f0534926111fcc8a4b962
irvin99 <54644054+irvin1999@users.noreply.github.com>
2023-06-10 13:39:03 +0200

    asdsad

--- php/form-process.php
+++ php/form-process.php
@@ -1,29 +1,41 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recibe los datos del formulario
-    $date = $_POST["date"];
-    $time = $_POST["time"];
-    $person = $_POST["person"];
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
 
-    // Configura los detalles del correo electrónico
-    $to = "11snaider99@gmail.com";
-    $subject = "Reserva de mesa";
-    $message = "Fecha: " . $date . "\n";
-    $message .= "Hora: " . $time . "\n";
-    $message .= "Número de personas: " . $person . "\n";
-    $message .= "Nombre: " . $name . "\n";
-    $message .= "Correo electrónico: " . $email . "\n";
-    $message .= "Teléfono: " . $phone . "\n";
-    $headers = "From: " . $email;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $correo = $_POST['email'];
+    $nombre = $_POST['name'];
+    $mensaje = "Fecha: ".$_POST['date']."<br>"."Hora: ".$_POST['time']."<br>"."Personas: ".$_POST['person'];
 
-    // Envía el correo electrónico
-    if (mail($to, $subject, $message, $headers)) {
-        echo "¡Gracias por reservar! Te contactaremos pronto.";
+    //echo $correo . " " . $nombre . " " . $mensaje;
+
+    $destinatario = "11snaider99@gmail.com";
+    $asunto = "Reserva de mesa"; 
+    $cuerpo = '
+        <html> 
+            <head> 
+                <title>Reserva de mesa</title> 
+            </head>
+            <body> 
+                <h1>Reserva de mesa</h1>
+                <p> 
+                    Nombre: '.$nombre.'<br>
+                    Correo: '.$correo.'<br>
+                    Reserva:<br>'.$mensaje.' 
+                </p> 
+            </body>
+        </html>
+    ';
+    //para el envío en formato HTML 
+    $headers = "MIME-Version: 1.0\r\n"; 
+    $headers .= "Content-type: text/html; charset=UTF8\r\n"; 
+
+    //dirección del remitente
+    $headers .= "From: $nombre <$correo>\r\n";
+    
+    if (mail($destinatario, $asunto, $cuerpo, $headers)) {
+        echo "Correo enviado";
     } else {
-        echo "Error al enviar el formulario. Por favor, inténtalo de nuevo.";
+        echo "Error al enviar el correo";
     }
 }
 ?>
+<a href="reservation.html">Volver a inicio</a>
